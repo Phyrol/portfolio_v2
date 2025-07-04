a0d3c6aa8f5dd8f140d8bda802fb6f10d09c1f19
Bradley Potzka <bpotzka@amazon.com>
2025-07-04 13:24:46 -0400

    refactor: replace react-scripts with webpack

---
+++ jest.config.js
@@ -0,0 +1,27 @@
+module.exports = {
+    globals: {
+        "__DEV__": true,
+    },
+    testEnvironment: "jsdom",
+    setupFilesAfterEnv: ["<rootDir>/src/setupTests.ts"],
+    testRegex: "\\.test.(tsx|ts)?$",
+    collectCoverage: true,
+    coverageDirectory: "build/coverage",
+    collectCoverageFrom: ["<rootDir>/**/*.{ts,tsx}"],
+    coveragePathIgnorePatterns: [
+        "/node_modules/",
+        "/__tests__/",
+    ],
+    coverageReporters: ["json", "lcov", "text", "text-summary", "cobertura", "html"],
+    coverageThreshold: {
+        global: {
+            statements: 75,
+            branches: 75,
+            functions: 75,
+            lines: 75,
+        },
+    },
+    reporters: ["default"],
+    moduleFileExtensions: ["ts", "tsx", "js", "json"],
+    testTimeout: 30000,
+};
---
+++ webpack.config.js
@@ -0,0 +1,54 @@
+const path = require("path");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = (env, argv) => {
+  return {
+    mode: argv.mode || 'development',
+    entry: {
+      main: path.resolve(__dirname, 'src/index.tsx'),
+    },
+    output: {
+      path: path.resolve(__dirname, 'public'),
+    },
+    resolve: {
+      extensions: ['.tsx', '.ts', '.jsx', '.js'],
+    },
+    module: {
+      rules: [
+            {
+      test: /\.[jt]sx?$/,
+      exclude: /node_modules/,
+      include: [path.resolve(__dirname, "src")],
+      use: {
+        loader: 'babel-loader',
+        options: {
+           presets: [
+        '@babel/preset-env',
+        '@babel/preset-react',
+        '@babel/preset-typescript',
+      ],
+        },
+      },
+    },
+    {
+      test: /\.css$/i,
+      use: ['style-loader', 'css-loader'],
+    },
+    {
+      test: /\.svg$/,
+      type: 'asset/resource', // emits the file
+    },
+      ],
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: './public/index.html',
+      }),
+    ],
+    devServer: {
+      static: './dist',
+      hot: true,
+      port: 3000,
+    },
+  };
+};
