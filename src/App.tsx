fe483fb1111501b927267681b526a4b4b600691d
Bradley Potzka <bpotzka@amazon.com>
2025-07-06 19:27:40 -0400

    chore: setup basic theme and styling
    - add a theme to add custom colors
    - add basic styling for body, h1/2/3/4
    - remove template react stuff
    - add basic Home page and some initial info

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,10 @@
-import logo from "./logo.svg";
+import Home from "./Pages/Home";
 import "./App.css";
 
 const App = () => {
     return (
-        <div className="app">
-            <header className="app-header">
-                <img src={logo} className="app-logo" alt="logo" />
-                <h1 className="text-3xl font-bold text-red-300 underline">Hello World!</h1>
-                <p>
-                    Edit <code>src/App.js</code> and save to reload.
-                </p>
-                <a className="app-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-                    Learn React
-                </a>
-            </header>
+        <div>
+            <Home />
         </div>
     );
 };
