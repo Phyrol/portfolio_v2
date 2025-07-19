1e60fa1357848053c04757a2fb915043d6043f92
Bradley Potzka <bpotzka@amazon.com>
2025-07-19 18:56:06 -0400

    style: experience card add inset shadow on hover

--- src/pages/Home/components/Experience/Experience.tsx
+++ src/pages/Home/components/Experience/Experience.tsx
@@ -99,7 +99,7 @@ const Experience = () => {
                 <Wrapper
                     key={`work-info-${index}`}
                     {...(isWide ? {href: headerInfo.link, target: "_blank", rel: "noreferrer"} : {})}
-                    className={`block ${isWide ? `hover:bg-space-cadet/60 hover:cursor-pointer group/${EXPERIENCE_GROUP_NAME}` : ""} mb-1 space-y-1 rounded-md p-4`}
+                    className={`block ${isWide ? `hover:bg-space-cadet/60 hover:cursor-pointer group/${EXPERIENCE_GROUP_NAME} hover:inset-shadow-lightest-space-cadet hover:inset-shadow-sm` : ""} mb-1 space-y-1 rounded-md p-4`}
                 >
                     <WorkInfoHeader {...headerInfo} />
 
