f91d5a00283b566c20a88a7b54cf46db5c02b5ef
Bradley Potzka <bpotzka@amazon.com>
2025-08-10 16:02:13 -0400

    add test for App.tsx

--- src/__tests__/App.test.tsx
+++ src/__tests__/App.test.tsx
@@ -1,6 +1,11 @@
 import {render, screen} from "@testing-library/react";
 import App from "../App";
+import {HomePageSection} from "pages/Home";
 
-test("renders learn react link", () => {
-    render(<App />);
+describe("App", () => {
+    it("should render the app on the home page", () => {
+        render(<App />);
+
+        expect(screen.getByTestId(HomePageSection.ABOUT)).toBeVisible();
+    });
 });
